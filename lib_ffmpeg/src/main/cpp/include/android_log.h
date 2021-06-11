71de2d66e077e5246781c2e536b1eb26372e578e
Sptlt <1037050764@qq.com>
2021-06-11 17:53:04 +0800

    简单的性能优化

--- lib_ffmpeg/src/main/cpp/include/android_log.h
+++ lib_ffmpeg/src/main/cpp/include/android_log.h
@@ -16,7 +16,7 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
 #define LOGVX(tag, ...) if(IS_DEBUG)__android_log_print(ANDROID_LOG_VERBOSE, tag, __VA_ARGS__)
-#define LOGDX(tag, ...) if(IS_DEBUG)__android_log_print(ANDROID_LOG_DEBUG, tag, __VA_ARGS__)
+#define LOGDX(tag, ...) if(IS_DEBUG){__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__);}
 #define LOGWX(tag, ...) __android_log_print(ANDROID_LOG_WARN, tag, __VA_ARGS__)
 #define LOGEX(tag, ...) __android_log_print(ANDROID_LOG_ERROR, tag, __VA_ARGS__)
 #define LOGIX(tag, ...) __android_log_print(ANDROID_LOG_INFO, tag, __VA_ARGS__)
--- lib_ffmpeg/src/main/cpp/player/Muxer.c
+++ lib_ffmpeg/src/main/cpp/player/Muxer.c
@@ -2,6 +2,7 @@
 // Created by LN on 2021/1/13.
 //
 #include "include/Muxer.h"
+
 #define LOG_TAG "Muxer------------->"
 
 static bool isMuxing = false;
@@ -22,6 +23,47 @@ ReleaseResource(AVFormatContext **in_fmt1, AVFormatContext **in_fmt2, AVFormatCo
     }
 }
 
+/**
+                * 时间基(time_base)是时间戳(timestamp)的单位，时间戳值乘以时间基，可以得到实际的时刻值(以秒等为单位)。
+                * 时间基可以理解为将1s平均分成若干份，如AV_TIME_BASE=1000000，就是将1s分成1000000份，如果帧率为25，那么每一帧的间隔（也是时间戳的间隔）为：1000000*（1、25）
+                * 例如，如果一个视频帧的 dts 是 40，pts 是 160，其 time_base 是 1/1000 秒，那么可以计算出此视频帧的解码时刻是 40 毫秒(40/1000)，显示时刻是 160 毫秒(160/1000)。
+                * FFmpeg 中时间戳(pts/dts)的类型是 int64_t 类型，把一个 time_base 看作一个时钟脉冲，则可把 dts/pts 看作时钟脉冲的计数。
+                * 三种时间基 tbr、tbn 和 tbc
+                * 不同的封装格式具有不同的时间基。在 FFmpeg 处理音视频过程中的不同阶段，也会采用不同的时间基。
+                * FFmepg 中有三种时间基，命令行中 tbr、tbn 和 tbc 的打印值就是这三种时间基的倒数：
+                * tbn：对应容器中的时间基。值是 AVStream.time_base 的倒数
+                * tbc：对应编解码器中的时间基。值是 AVCodecContext.time_base 的倒数
+                * tbr：从视频流中猜算得到，可能是帧率或场率(帧率的 2 倍)
+                *
+                * 除以上三种时间基外，FFmpeg 还有一个内部时间基 AV_TIME_BASE(以及分数形式的 AV_TIME_BASE_Q)
+                * AV_TIME_BASE 及 AV_TIME_BASE_Q 用于 FFmpeg 内部函数处理，使用此时间基计算得到时间值表示的是微秒
+                *
+                * 时间值形式转换
+                * av_q2d()将时间从 AVRational 形式转换为 double 形式。AVRational 是分数类型，double 是双精度浮点数类型，转换的结果单位是秒。转换前后的值基于同一时间基，仅仅是数值的表现形式不同而已。
+                *
+                * av_rescale_q()用于不同时间基的转换，用于将时间值从一种时间基转换为另一种时间基
+                * av_packet_rescale_ts()用于将 AVPacket 中各种时间值从一种时间基转换为另一种时间基
+                *
+                * int64_t av_rescale_rnd(int64_t a, int64_t b, int64_t c, enum AVRounding rnd)
+                * 作用是计算 "a * b / c" 的值并分五种方式来取整
+                * 将以 "时钟基c" 表示的 数值a 转换成以 "时钟基b" 来表示
+                *
+                * AVStream.time_base 是 AVPacket 中 pts 和 dts 的时间单位，输入流与输出流中 time_base 按如下方式确定：
+                * 对于输入流：打开输入文件后，调用 avformat_find_stream_info()可获取到每个流中的 time_base
+                * 对于输出流：打开输出文件后，调用 avformat_write_header()可根据输出文件封装格式确定每个流的 time_base 并写入输出文件中
+                * 不同封装格式具有不同的时间基，在转封装(将一种封装格式转换为另一种封装格式)过程中，时间基转换相关代码如下
+                * 方式1：
+                * av_read_frame(ifmt_ctx, &pkt);
+                * pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+                * pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+                * pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+                * 方式2；
+                * av_read_frame(ifmt_ctx, &pkt);
+                * 将 packet 中的各时间值从输入流封装格式时间基转换到输出流封装格式时间基
+                * av_packet_rescale_ts(&pkt, in_stream->time_base, out_stream->time_base);
+                * 这里流里的时间基in_stream->time_base和out_stream->time_base，是容器中的时间基，就是 tbn
+                *
+*/
 int MuxAVFile(char *audio_srcPath, char *video_srcPath, char *destPath) {
     if (isMuxing) {
         LOGE("is under muxing file!,please waiting");
@@ -171,12 +213,7 @@ int MuxAVFile(char *audio_srcPath, char *video_srcPath, char *destPath) {
                                                          audio_ou_stream->time_base, AV_ROUND_UP);
 
                 audioPacket->pos = -1;
-                /*AVRational tb = audio_ou_stream->time_base;
-                  LOGDX(LOG_TAG, "audio pts:%s dts:%s duration %s size %d finish %d",
-                  av_ts2timestr(audioPacket->pts, &tb),
-                  av_ts2timestr(audioPacket->dts, &tb),
-                  av_ts2timestr(audioPacket->duration, &tb),
-                  audioPacket->size, audio_finish);*/
+
             }
         }
 
@@ -193,55 +230,12 @@ int MuxAVFile(char *audio_srcPath, char *video_srcPath, char *destPath) {
 
             if (!video_finish) {
                 found_video = true;
-                /**
-                 * 时间基(time_base)是时间戳(timestamp)的单位，时间戳值乘以时间基，可以得到实际的时刻值(以秒等为单位)。
-                 * 时间基可以理解为将1s平均分成若干份，如AV_TIME_BASE=1000000，就是将1s分成1000000份，如果帧率为25，那么每一帧的间隔（也是时间戳的间隔）为：1000000*（1、25）
-                 * 例如，如果一个视频帧的 dts 是 40，pts 是 160，其 time_base 是 1/1000 秒，那么可以计算出此视频帧的解码时刻是 40 毫秒(40/1000)，显示时刻是 160 毫秒(160/1000)。
-                 * FFmpeg 中时间戳(pts/dts)的类型是 int64_t 类型，把一个 time_base 看作一个时钟脉冲，则可把 dts/pts 看作时钟脉冲的计数。
-                 * 三种时间基 tbr、tbn 和 tbc
-                 * 不同的封装格式具有不同的时间基。在 FFmpeg 处理音视频过程中的不同阶段，也会采用不同的时间基。
-                 * FFmepg 中有三种时间基，命令行中 tbr、tbn 和 tbc 的打印值就是这三种时间基的倒数：
-                 * tbn：对应容器中的时间基。值是 AVStream.time_base 的倒数
-                 * tbc：对应编解码器中的时间基。值是 AVCodecContext.time_base 的倒数
-                 * tbr：从视频流中猜算得到，可能是帧率或场率(帧率的 2 倍)
-                 *
-                 * 除以上三种时间基外，FFmpeg 还有一个内部时间基 AV_TIME_BASE(以及分数形式的 AV_TIME_BASE_Q)
-                 * AV_TIME_BASE 及 AV_TIME_BASE_Q 用于 FFmpeg 内部函数处理，使用此时间基计算得到时间值表示的是微秒
-                 *
-                 * 时间值形式转换
-                 * av_q2d()将时间从 AVRational 形式转换为 double 形式。AVRational 是分数类型，double 是双精度浮点数类型，转换的结果单位是秒。转换前后的值基于同一时间基，仅仅是数值的表现形式不同而已。
-                 *
-                 * av_rescale_q()用于不同时间基的转换，用于将时间值从一种时间基转换为另一种时间基
-                 * av_packet_rescale_ts()用于将 AVPacket 中各种时间值从一种时间基转换为另一种时间基
-                 *
-                 * int64_t av_rescale_rnd(int64_t a, int64_t b, int64_t c, enum AVRounding rnd)
-                 * 作用是计算 "a * b / c" 的值并分五种方式来取整
-                 * 将以 "时钟基c" 表示的 数值a 转换成以 "时钟基b" 来表示
-                 *
-                 * AVStream.time_base 是 AVPacket 中 pts 和 dts 的时间单位，输入流与输出流中 time_base 按如下方式确定：
-                 * 对于输入流：打开输入文件后，调用 avformat_find_stream_info()可获取到每个流中的 time_base
-                 * 对于输出流：打开输出文件后，调用 avformat_write_header()可根据输出文件封装格式确定每个流的 time_base 并写入输出文件中
-                 * 不同封装格式具有不同的时间基，在转封装(将一种封装格式转换为另一种封装格式)过程中，时间基转换相关代码如下
-                 * 方式1：
-                 * av_read_frame(ifmt_ctx, &pkt);
-                 * pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-                 * pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-                 * pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-                 * 方式2；
-                 * av_read_frame(ifmt_ctx, &pkt);
-                 * 将 packet 中的各时间值从输入流封装格式时间基转换到输出流封装格式时间基
-                 * av_packet_rescale_ts(&pkt, in_stream->time_base, out_stream->time_base);
-                 * 这里流里的时间基in_stream->time_base和out_stream->time_base，是容器中的时间基，就是 tbn
-                 *
-                 */
-
                 videoPacket->stream_index = video_out_stream->index;
                 if (videoPacket->dts == AV_NOPTS_VALUE) {
                     LOGDX(LOG_TAG, "video pkt not have pts,need calculate by frame rate");
                     AVRational o_time_base = video_out_stream->time_base;
                     int64_t frame_duration =
                             (double) AV_TIME_BASE / av_q2d(video_in_stream->r_frame_rate);
-
                     //计算出时间基并并转为out stream中对应的时间基，不同的封装格式通常会有不同的时间紧
                     videoPacket->pts = (double) (video_pkt_index * frame_duration) /
                                        (double) (av_q2d(o_time_base) * AV_TIME_BASE);
