72c964fb1edb91d507fc6754a067fdcd9f220ab3
Sptlt <1037050764@qq.com>
2021-08-27 17:23:19 +0800

    add a MediaDecoder.cpp

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -101,14 +101,7 @@ build()
     --enable-pic \
     --enable-jni \
     --enable-shared \
-    --enable-mediacodec \
     --disable-decoders \
-    --enable-decoder=aac \
-    --enable-decoder=aac_latm \
-    --enable-decoder=mjpeg \
-    --enable-decoder=png \
-    --enable-decoder=h264_mediacodec \
-    --enable-decoder=mpeg4_mediacodec \
 
     echo "----------------configure finish!--------------------"
     make -j8
